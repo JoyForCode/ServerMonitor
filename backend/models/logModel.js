8b03e9898739e7932949b5b037065bb7c0fb349c
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2025-10-31 23:05:08 +0530

    edited the module export statement , fixed typo in the collections name

--- backend/models/logModel.js
+++ backend/models/logModel.js
@@ -14,4 +14,4 @@ const LogSchema = new mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('Log', LogSchema, 'statuslogs');
+module.exports = mongoose.model('Log', LogSchema, 'statusLogs');
